f79660fd3b5dc5782eabba57259f3261e66f4465
Satyam Kumar <satyamraj72261@gmail.com>
2026-05-02 06:20:29 +0530

    fix: updated API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
-  "https://smart-text-bot-backend-docker.onrender.com";
+const API_BASE_URL = "https://smart-text-bot-backend-docker.onrender.com";
 
 console.log("API BASE URL:", API_BASE_URL);
 
